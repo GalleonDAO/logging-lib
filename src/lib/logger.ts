260f02bbd9285f693619dca2509dc19e9afef006
crown_dev <crown@galleon.community>
2022-06-12 10:43:20 +0100

    Move logger to FaF transports

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -2,103 +2,52 @@ import {
   BASE_URL,
   COUNTERS_ENDPOINT,
   LOGS_ENDPOINT,
-  LOG_SEVERITY,
   SUBSCRIPTION_HEADER_KEY,
   TIMERS_ENDPOINT,
   TRACE_KEY,
 } from './constants'
+import { ConsoleTransport, MonitoringAPITransport } from './transports'
 
 class Logger {
-  constructor(apiKey: string) {
+  constructor(apiKey: string, useConsoleTransport: boolean = false) {
     this.key = apiKey
     this.headers = new Headers()
     this.headers.set(SUBSCRIPTION_HEADER_KEY, this.key)
     this.headers.set(TRACE_KEY, true.toString())
     this.headers.set('Content-Type', 'application/json')
+
+    this.transports.push(new MonitoringAPITransport({
+      loggerUrl: BASE_URL,
+      logsEndpoint: LOGS_ENDPOINT,
+      countersEndpoint: COUNTERS_ENDPOINT,
+      timersEndpoint: TIMERS_ENDPOINT,
+      headers: this.headers
+    }));
+
+    if(useConsoleTransport)
+      this.transports.push(new ConsoleTransport());
   }
 
   key: string
   headers: HeadersInit
+  transports: ILoggingTransport[]
 
-  logMessage = async (payload: MessagePayload) => {
-    const loggerUrl = BASE_URL + LOGS_ENDPOINT
-    return await fetch(loggerUrl, {
-      method: 'POST',
-      headers: this.headers,
-      body: JSON.stringify({
-        ServiceName: payload.serviceName,
-        Environment: payload.environment,
-        TimeStamp: payload.timestamp,
-        Severity: payload.severity,
-        FunctionName: payload.functionName,
-        Exception: payload.exception,
-        Message: payload.message,
-        CorrelationID: payload.correlationId,
-      }),
-    }).catch((err) => {
-      console.log(err, {
-        serviceName: payload.serviceName,
-        environment: payload.environment,
-        timestamp: new Date().toISOString(),
-        severity: LOG_SEVERITY.ERROR,
-        functionName: 'logMessage',
-        exception: err.stack,
-        message: err.message,
-        correlationId: payload.correlationId,
-      })
-    })
+  logMessage = (payload: MessagePayload) => {    
+    this.transports.forEach(transport => {
+      transport.logMessageAsync(payload);
+    });
   }
 
-  logCounter = async (payload: CounterPayload) => {
-    const loggerUrl = BASE_URL + COUNTERS_ENDPOINT
-    return await fetch(loggerUrl, {
-      method: 'POST',
-      headers: this.headers,
-      body: JSON.stringify({
-        ServiceName: payload.serviceName,
-        Environment: payload.environment,
-        Label: payload.label,
-        Metadata: payload.metadata,
-      }),
-    }).catch((err) => {
-      console.log(err)
-      this.logMessage({
-        serviceName: payload.serviceName,
-        environment: payload.environment,
-        timestamp: new Date().toISOString(),
-        severity: LOG_SEVERITY.ERROR,
-        functionName: 'logCounter',
-        exception: err.stack,
-        message: err.message,
-        correlationId: undefined,
-      })
-    })
+  logCounter = (payload: CounterPayload) => {
+    this.transports.forEach(transport => {
+      transport.logCounterAsync(payload);
+    });
   }
 
-  logTimer = async (payload: TimerPayload) => {
-    const loggerUrl = BASE_URL + TIMERS_ENDPOINT
-    return await fetch(loggerUrl, {
-      method: 'POST',
-      headers: this.headers,
-      body: JSON.stringify({
-        ServiceName: payload.serviceName,
-        Environment: payload.environment,
-        Label: payload.label,
-        Duration: payload.duration,
-      }),
-    }).catch((err) => {
-      console.log(err)
-      this.logMessage({
-        serviceName: payload.serviceName,
-        environment: payload.environment,
-        timestamp: new Date().toISOString(),
-        severity: LOG_SEVERITY.ERROR,
-        functionName: 'logTimer',
-        exception: err.stack,
-        message: err.message,
-        correlationId: undefined,
-      })
-    })
+  logTimer = (payload: TimerPayload) => {
+    this.transports.forEach(transport => {
+      transport.logTimerAsync(payload);
+    });
   }
 }
 
