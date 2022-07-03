4305e950f7f9fc67da711074efbfa1be5ee3d560
crown_dev <crown@galleon.community>
2022-07-03 12:44:29 +0100

    fix: logger export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-import Logger from './lib/logger'
-export * from './lib/constants'
-export default Logger
+import Logger from "./lib/logger";
+import LoggerV2 from "./lib/loggerV2";
+export * from "./lib/constants";
+export default Logger;
+export const LoggingV2 = LoggerV2; //TODO:Remove
