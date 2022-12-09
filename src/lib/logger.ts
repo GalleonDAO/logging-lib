c127a2a046f16bc64905a2ab92c492b261332c50
crown_dev <crown@galleon.community>
2022-12-09 16:40:19 +0000

    Adds sessionID and address obfuscation

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -5,14 +5,21 @@ import {
   TIMERS_ENDPOINT,
 } from "./constants";
 import { ConsoleTransport, MonitoringAPITransport } from "./transports";
+import { Guid } from "guid-typescript";
 
 /**
  * DEPRECATED: will be removed once versioning is handled
  */
 class Logger {
   transports: ILoggingTransport[];
+  private sessionId: Guid;
 
-  constructor(apiKey: string, useConsoleTransport: boolean = false) {
+  constructor(
+    apiKey: string,
+    sessionId: Guid = Guid.create(),
+    useConsoleTransport: boolean = false
+  ) {
+    this.sessionId = sessionId;
     this.transports = [];
 
     this.transports.push(
@@ -35,6 +42,19 @@ class Logger {
   };
 
   logCounter = (payload: CounterPayload) => {
+    if (payload.metadata) {
+      if (payload.metadata["address"]) {
+        payload.metadata["address"] = this.redactAddress(
+          payload.metadata["address"]
+        );
+      }
+      payload.metadata["sessionId"] = this.sessionId;
+    } else {
+      payload.metadata = {
+        sessionId: this.sessionId,
+      };
+    }
+
     this.transports.forEach((transport) => {
       transport.logCounterAsync(payload);
     });
@@ -44,6 +64,12 @@ class Logger {
     this.transports.forEach((transport) => {
       transport.logTimerAsync(payload);
     });
+  };
+
+  //Returns a formatted address where first and last 4 chars are preserved
+  //Centre of address is redacted
+  private redactAddress = (address: string): string => {
+    return address.replace("(w{6})(w{32})(w{6})", "$1******$3");
   };
 }
 
